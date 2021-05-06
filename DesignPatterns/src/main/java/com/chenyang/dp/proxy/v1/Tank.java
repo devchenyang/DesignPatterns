24f2d303e88330f106fa5f8d207c9590ebcf4300
devchenyang <ethanchen657@gmail.com>
2021-05-07 07:33:33 +0800

    [陈洋][20210507]通过修改源码记录时间日志

--- DesignPatterns/src/main/java/com/chenyang/dp/proxy/v1/Tank.java
+++ DesignPatterns/src/main/java/com/chenyang/dp/proxy/v1/Tank.java
@@ -9,6 +9,8 @@ public class Tank implements Movable {
     @Override
     public void move() {
 
+        long start = System.currentTimeMillis();
+
         System.out.println("Tank moving claclacla...");
         try {
             Thread.sleep(10000);
@@ -16,5 +18,12 @@ public class Tank implements Movable {
             e.printStackTrace();
         }
 
+        long end = System.currentTimeMillis();
+
+        System.out.println(String.format("执行时间：%d毫秒", end - start));
+    }
+
+    public static void main(String[] args) {
+        new Tank().move();
     }
 }
