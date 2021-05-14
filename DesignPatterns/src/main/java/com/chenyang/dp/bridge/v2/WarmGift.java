0a0588b5a40c24c6f8abc96e50d83fe5db06b390
devchenyang <ethanchen657@gmail.com>
2021-05-14 19:42:58 +0800

    [陈洋][20210514]通过构造方法给Gift聚合的GiftImpl赋值

--- DesignPatterns/src/main/java/com/chenyang/dp/bridge/v2/WarmGift.java
+++ DesignPatterns/src/main/java/com/chenyang/dp/bridge/v2/WarmGift.java
@@ -1,4 +1,7 @@
 package com.chenyang.dp.bridge.v2;
 
 public class WarmGift extends Gift {
+    public WarmGift(GiftImpl giftImpl) {
+        this.giftImpl = giftImpl;
+    }
 }
--- DesignPatterns/src/main/java/com/chenyang/dp/bridge/v2/WildGift.java
+++ DesignPatterns/src/main/java/com/chenyang/dp/bridge/v2/WildGift.java
@@ -1,4 +1,7 @@
 package com.chenyang.dp.bridge.v2;
 
 public class WildGift extends Gift {
+    public WildGift(GiftImpl giftImpl) {
+        this.giftImpl = giftImpl;
+    }
 }
