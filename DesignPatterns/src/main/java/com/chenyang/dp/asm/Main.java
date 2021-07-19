9400a106444b485c684e2b2e9c967f868155cd8a
devchenyang <83169752+devchenyang@users.noreply.github.com>
2021-07-19 17:30:43 +0800

    [陈洋][20210519]添加测试类

---
+++ DesignPatterns/src/main/java/com/chenyang/dp/asm/Main.java
@@ -0,0 +1,11 @@
+package com.chenyang.dp.asm;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Object o = new Object();
+        AtomicInteger atomicInteger = new AtomicInteger();
+        int num = 0;
+    }
+}
